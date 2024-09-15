bb1ce24e77897ecc819e6b45378352dd18355ed9
ckarul <cuneyt@karul.org>
2024-09-15 17:41:49 -0400

    Update improper.c

--- improper.c
+++ improper.c
@@ -1,11 +1,44 @@
+include <string.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 
+#define BUFSIZE 256
+
+// This program prints the size of a specified file in bytes
+
+ 
 int main(int argc, char** argv) {
-  char *userName = argv[1];
-  
-    char command1[1000] = {0};
-    sprintf(command1, "ls %s", userName);
-    system(command1);
+ 
+void encodeShellString(char str1[], int num, char str2[]) {
+ 
+// This is a shell for a function that would validate the input 
+ 
+// escape any special characters
+ 
+// and enclose in quotes
 }
+
+ 
+// Ensure that the user supplied exactly one command line argument
+ 
+if (argc != 2) { 
+ 
+fprintf(stderr, "Please provide the address of a file as an input.\n");
+ 
+return -1;
+ 
+}
+ 
+char *fileName = argv[1];
+ 
+char userFileQuoted[BUFSIZE] = {0};
+
+encodeShellString(userFileQuoted, BUFSIZE, fileName); 
+
+char command[BUFSIZE] = {0};
+
+snprintf(command, BUFSIZE, "wc -c < %s", userFileQuoted);
+
+system(command);
+
+return 0;
