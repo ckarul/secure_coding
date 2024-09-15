b0708c1c2e99c583d411f7069105ea68a152eb72
ckarul <cuneyt@karul.org>
2024-09-15 17:55:54 -0400

    Update improper.c

--- improper.c
+++ improper.c
@@ -6,34 +6,22 @@
 
 // This program prints the size of a specified file in bytes
 
- 
 int main(int argc, char** argv) {
- 
 void encodeShellString(char str1[], int num, char str2[]) {
- 
-// This is a shell for a function that would validate the input 
- 
+// This is a shell for a function that would validate the inpu
 // escape any special characters
- 
 // and enclose in quotes
 }
 
- 
 // Ensure that the user supplied exactly one command line argument
- 
-if (argc != 2) { 
- 
+if (argc != 2)
 fprintf(stderr, "Please provide the address of a file as an input.\n");
- 
 return -1;
- 
 }
- 
 char *fileName = argv[1];
- 
 char userFileQuoted[BUFSIZE] = {0};
 
-encodeShellString(userFileQuoted, BUFSIZE, fileName); 
+encodeShellString(userFileQuoted, BUFSIZE, fileName);
 
 char command[BUFSIZE] = {0};
 
